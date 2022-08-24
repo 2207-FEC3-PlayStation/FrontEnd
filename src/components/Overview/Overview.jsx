2ea2c13a4117f2c85bfe8542b700b552716e7b14
Ricardo Vargas <richi_v@aol.com>
2022-08-24 16:32:32 -0500

    Added a class and state to overview.jsx

---
+++ src/components/Overview/Overview.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+class Overview extends React.component {
+  constructor(props) {
+    super(props)
+
+    this.state{
+      list: []
+    }
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+
+
+
+
