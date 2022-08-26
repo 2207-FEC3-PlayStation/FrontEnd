11465f49fc9140fb4b0e3fb63ee7967327a3af89
Ricardo Vargas <richi_v@aol.com>
2022-08-26 16:44:17 -0500

    added import styles to overview.jsx and imageGallery.jsx

--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -1,16 +1,79 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import ImageGallery from './ImageGallery.jsx'
 import ProductInfo from './ProductInfo.jsx'
 import StyleSelect from './StyleSelect.jsx'
 import CheckOut from './CheckOut.jsx'
 import axios from 'axios'
+import styled from 'styled-components'
 
-//product={prod} setProd={setProd}
-class Overview extends React.Components {
+
+class Overview extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      dummy: []
+      dummy: {
+        "product_id": "66642",
+        "results": [
+          {
+            "style_id": 411534,
+            "name": "Forest Green & Black",
+            "original_price": "140.00",
+            "sale_price": null,
+            "default?": true,
+            "photos": [
+              {
+                "thumbnail_url": "https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+                "url": "https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
+              },
+              {
+                "thumbnail_url": "https://images.unsplash.com/photo-1534011546717-407bced4d25c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+                "url": "https://images.unsplash.com/photo-1534011546717-407bced4d25c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2734&q=80"
+              },
+              {
+                "thumbnail_url": "https://images.unsplash.com/photo-1549831243-a69a0b3d39e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+                "url": "https://images.unsplash.com/photo-1549831243-a69a0b3d39e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2775&q=80"
+              },
+              {
+                "thumbnail_url": "https://images.unsplash.com/photo-1527522883525-97119bfce82d?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
+                "url": "https://images.unsplash.com/photo-1527522883525-97119bfce82d?ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80"
+              },
+              {
+                "thumbnail_url": "https://images.unsplash.com/photo-1556648202-80e751c133da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+                "url": "https://images.unsplash.com/photo-1556648202-80e751c133da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
+              },
+              {
+                "thumbnail_url": "https://images.unsplash.com/photo-1532543491484-63e29b3c1f5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+                "url": "https://images.unsplash.com/photo-1532543491484-63e29b3c1f5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80"
+              }
+            ],
+            "skus": {
+              "2390357": {
+                "quantity": 8,
+                "size": "XS"
+              },
+              "2390358": {
+                "quantity": 16,
+                "size": "S"
+              },
+              "2390359": {
+                "quantity": 17,
+                "size": "M"
+              },
+              "2390360": {
+                "quantity": 10,
+                "size": "L"
+              },
+              "2390361": {
+                "quantity": 15,
+                "size": "XL"
+              },
+              "2390362": {
+                "quantity": 4,
+                "size": "XL"
+              }
+            }
+          }]
+      }
     }
   }
 
@@ -18,16 +81,16 @@ class Overview extends React.Components {
 
 
   render() {
-  return (
-    <div>
-      <h1>Image Gallery</h1>
-      <ImageGallery />
-      <ProductInfo />
-      <StyleSelect />
-      <CheckOut />
-    </div >
-  )
-}
+    return (
+      <div>
+        <h1>Image Gallery</h1>
+        <ImageGallery images={this.state.dummy} />
+        <ProductInfo />
+        <StyleSelect />
+        <CheckOut />
+      </div >
+    )
+  }
 }
 
 export default Overview;
