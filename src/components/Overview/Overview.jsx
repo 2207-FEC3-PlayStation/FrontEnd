cb98a7cbf090c083a87b268a885ded6eff32fe32
Ricardo Vargas <richi_v@aol.com>
2022-08-26 16:15:41 -0500

    Switched back to class for now

--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -1,36 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ImageGallery from './ImageGallery.jsx'
 import ProductInfo from './ProductInfo.jsx'
 import StyleSelect from './StyleSelect.jsx'
 import CheckOut from './CheckOut.jsx'
 import axios from 'axios'
 
-class Overview extends React.Component {
+//product={prod} setProd={setProd}
+class Overview extends React.Components {
   constructor(props) {
     super(props)
-
     this.state = {
-      list: [],
+      dummy: []
     }
   }
 
 
 
 
-
   render() {
-    return (
-      <div>
-        <h1>Image Gallery</h1>
-        <ImageGallery />
-        <ProductInfo />
-        <StyleSelect />
-        <CheckOut />
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h1>Image Gallery</h1>
+      <ImageGallery />
+      <ProductInfo />
+      <StyleSelect />
+      <CheckOut />
+    </div >
+  )
 }
-
+}
 
 export default Overview;
 
