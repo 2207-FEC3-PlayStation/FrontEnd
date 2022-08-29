91c99304909f5ab82414bf5f834bd5238e47d58b
Asael Gonzalez <agonzalezengr@gmail.com>
2022-08-29 15:43:27 -0500

    Completed CSS transfer to CSS files. Still reworking on Question :

--- src/components/QandA/QandA.jsx
+++ src/components/QandA/QandA.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import QuestionList from './QuestionList.jsx';
+import './QAstyles/QandA.css';
 
-//test?
+
 class QandA extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       search: '',
+      count: 4,
       combos: {
         questions: ['What is life?', 'When was was gum invented?', 'Why are there so many chickens?', 'Who are you?'],
-        answers: ['Life is everything.', 'Gum was invented in 1923.', 'Because KFC is delicious!', 'A am no one.']
+        answers: ['Life is everything.', 'Gum was invented in 1923.', 'Because KFC is delicious!', 'I am no one.'],
+        users: ['User1234', 'User2222', 'User4343', 'User0203'],
+        seller: [false, true, false, false],
+        date: ['Jan']
       }
     }
     this.handleSearch = this.handleSearch.bind(this)
@@ -24,6 +29,9 @@ class QandA extends React.Component {
 
   loadMoreAnswers(event) {
     event.preventDefault();
+    this.setState({
+      count: count + 1
+    })
     console.log('You clicked!');
   }
 
@@ -36,25 +44,22 @@ class QandA extends React.Component {
   render() {
     return (
       <div className="QandA">
-        <div id="Search" style={{border: 'medium solid black', backgroundColor: "aqua", width: "50%"}}>
-        <h2>QUESTIONS & ANSWERS</h2>
+        <div className="Search">
+        <h2> QUESTIONS & ANSWERS</h2>
         <input
+          className="search-bar"
           type="Text"
           onChange={this.handleSearch}
           placeholder="Have a question? Search for answers ..."
-          style={{width: "98%", display: "block", margin: "auto"}}
         />
         </div>
-        <QuestionList {...this.state.combos}/><br></br>
-        <button onClick={this.loadMoreAnswers} style ={{border: 'none', background: 'none'}}>
+        <QuestionList combos={this.state.combos} count={this.state.count}/><br></br>
+        <button id="load" onClick={this.loadMoreAnswers}>
           <b>LOAD MORE ANSWERS</b></button><br></br>
-
-        <button style={{margin: '10px', backgroundColor: "white", width: "230px", height: "50px"}}>
+        <button id="moreQA">
           <b>MORE ANSWERED QUESTIONS</b></button>
-
-        <button style={{margin: '10px', backgroundColor: "white", width: "150px", height: "50px"}}>
+        <button id="addQ">
           <b>ADD A QUESTION +</b></button>
-
       </div>
     )
   }
