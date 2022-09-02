c1b8e7ded66365bdae9797e1d8268ecaad50b159
Kevin Moyer <kevin7.moyer@gmail.com>
2022-09-02 09:42:46 -0500

    added a styled component to the productinfo page to allow the star component to properly size itself

--- src/components/RandR/RatingBreakdown/StarRating.jsx
+++ src/components/RandR/RatingBreakdown/StarRating.jsx
@@ -1,35 +1,22 @@
 import React, {useEffect, useState} from 'react';
 import server from '../../../serverRequests.js';
 
-function StarRating({avgRating, rating, product_id}) {
+function StarRating({avgRating, rating}) {
   //if a rating props is passed in, this component will take that value. Otherwise it will default to the average rating for the overall product
-  const [defaultAvg, setDefaultAvg] = useState();
+  const [avg, setAvg] = useState();
 
   useEffect(() => {
-    if (!avgRating && !rating && product_id) {
-      server.get('/reviews/meta', {'product_id': product_id})
-        .then((data) => {
-          let sum = 0;
-          let reviewCount = 0;
-          for(var key in data.data.ratings) {
-            let thisKey = parseInt(key);
-            let thisVal = parseInt(data.data.ratings[key]);
-            reviewCount += thisVal;
-            sum += thisVal * thisKey;
-          }
-          let average = (Math.round(4 * sum / reviewCount) / 4).toFixed(2);
-          let tenth = (Math.round(4 * sum / reviewCount) / 4).toFixed(1);
-          setDefaultAvg(average);
-        })
-
+    if (avgRating) {
+      setAvg(avgRating)
+    } else if (rating) {
+      setAvg(rating)
     }
-  }, [product_id])
+  }, [avgRating, rating])
 
 
-  let starRating = rating || avgRating || defaultAvg;
   return (
     <div className={'stars-outer'}>
-      <div className={'stars-inner'} style={{width: starRating*20 + '%'}}></div>
+      <div className={'stars-inner'} style={{width: (avg*20) + '%'}}></div>
     </div>
   )
 }
